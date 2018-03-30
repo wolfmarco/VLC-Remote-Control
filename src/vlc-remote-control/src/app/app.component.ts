c92e876b046d380e96871cf789eaeda6d26060c7
Marco Wolf <mail@wolfmarco.de>
2018-03-30 21:38:43 +0200

    - add globalShortcut example

--- src/vlc-remote-control/src/app/app.component.ts
+++ src/vlc-remote-control/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+
+  constructor(private _electronService: ElectronService) { }
+
+  ngOnInit(): void {
+    console.log("AppComponent: ngOnInit");
+
+    const ret = this._electronService.remote.globalShortcut.register('CommandOrControl+X', () => {
+      console.log('CommandOrControl+X is pressed')
+    });
+    if (!ret) {
+      console.log('registration failed')
+    }
+    console.log("isRegistered: " + this._electronService.remote.globalShortcut.isRegistered('CommandOrControl+X'));
+
+    this._electronService.remote.app.on('will-quit', () => {
+      this._electronService.remote.globalShortcut.unregisterAll();
+    })
+  }
 }
