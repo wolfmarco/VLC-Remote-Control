a0c537b6ab793cc431c4f23c0b58755311f4cba8
Marco Wolf <mail@wolfmarco.de>
2018-03-31 14:01:40 +0200

    - add keyboard event example

--- src/vlc-remote-control/src/app/app.component.ts
+++ src/vlc-remote-control/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 
 @Component({
@@ -10,21 +10,43 @@ export class AppComponent implements OnInit {
 
   title = 'app';
 
+  @ViewChild("text") text: ElementRef;
+
   constructor(private _electronService: ElectronService) { }
 
   ngOnInit(): void {
     console.log("AppComponent: ngOnInit");
+    if (this._electronService.isElectronApp)
+    {
+      const ret = this._electronService.remote.globalShortcut.register('CommandOrControl+X', () =>
+      {
+        console.log('CommandOrControl+X is pressed');
+      });
 
-    const ret = this._electronService.remote.globalShortcut.register('CommandOrControl+X', () => {
-      console.log('CommandOrControl+X is pressed')
-    });
-    if (!ret) {
-      console.log('registration failed')
+      if (!ret)
+      {
+        console.log('registration failed');
+      }
+
+      console.log("isRegistered: " + this._electronService.remote.globalShortcut.isRegistered('CommandOrControl+X'));
+
+      this._electronService.remote.app.on('will-quit', () =>
+      {
+        this._electronService.remote.globalShortcut.unregisterAll();
+      });
     }
-    console.log("isRegistered: " + this._electronService.remote.globalShortcut.isRegistered('CommandOrControl+X'));
+  }
 
-    this._electronService.remote.app.on('will-quit', () => {
-      this._electronService.remote.globalShortcut.unregisterAll();
-    })
+  onKeyup(event: any) {
+    console.log("altKey: " + event.altKey);
+    console.log("ctrlKey: " + event.ctrlKey);
+    console.log("shiftKey: " + event.shiftKey);
+    console.log("key: " + event.key );
+    console.log("code: " + event.code );
+    console.log("-----------------------");
+
+    this.text.nativeElement.value = `${event.code}\n`;
   }
 }
+
+
