9e177c4d7532aedcba10de73cdb16a710238d97a
Marco Wolf <mail@wolfmarco.de>
2018-04-01 13:18:24 +0200

    - add Electron.NET project and include angular CLI project into wwwroot folder

---
+++ src/vlc-remote-control-desktop/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+
+namespace vlc_remote_control_desktop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            
+            var defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("index.html");
+            app.UseDefaultFiles(defaultFilesOptions);
+
+            app.UseStaticFiles();
+
+            Task.Run(async () =>
+            {
+                var options = new BrowserWindowOptions()
+                {
+                };
+                await Electron.WindowManager.CreateWindowAsync();
+            });
+        }
+    }
+}
