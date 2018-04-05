a81ee79a2818a63abfddd0b9f975d8e180b6911d
Marco Wolf <mail@wolfmarco.de>
2018-04-05 18:41:42 +0200

    - code cleanup

--- build.cake
+++ build.cake
@@ -3,48 +3,11 @@
 var target = Argument("target", "Default");
 DirectoryPath electronDirectory = "./src/vlc-remote-control/";
 
-Task("clean")
-  .Does(() =>
-{
-  try
-  {
-    DeleteDirectories(new DirectoryPath[]
-    {
-      electronDirectory.Combine("obj"),
-      electronDirectory.Combine("bin")
-    }, new DeleteDirectorySettings
-    {
-      Recursive = true,
-      Force = true
-    });
-  }
-  catch(Exception exception)
-  {
-    Information( exception.ToString() );
-  }
-});
-
-Task("restore")
-.Does (() =>
-{
-  DotNetCoreRestore(new DotNetCoreRestoreSettings
-  {
-    WorkingDirectory = electronDirectory
-  });
-});
-
 Task("build")
   .Does(() =>
 {
   Yarn.FromPath(electronDirectory).Install();
   Yarn.FromPath(electronDirectory).RunScript("build");
-
-  // var exitCodeWithArgument = StartProcess("dotnet", new ProcessSettings
-  // { 
-  //   Arguments = "electronize start",
-  //   WorkingDirectory = electronDirectory
-  // });
-  // Information("Exit code: {0}", exitCodeWithArgument);
 });
 
 Task("Default")
--- src/vlc-remote-control/main.js
+++ src/vlc-remote-control/main.js
@@ -4,35 +4,54 @@ const url = require('url')
 
 let win
 
-function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600})
+const isWindows = process.platform === "win32";
+const isMac = process.platform === "darwin";
+
+function createWindow ()
+{
+  win = new BrowserWindow
+  ({
+    fullscreen: true,
+    autoHideMenuBar: true
+  });
 
   // load the dist folder from Angular
-  win.loadURL(url.format({
+  win.loadURL(url.format
+  ({
     pathname: path.join(__dirname, 'build/index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Open the DevTools optionally:
   win.webContents.openDevTools()
 
-  win.on('closed', () => {
+  win.on('closed', () =>
+  {
     win = null
-  })
+  });
 }
 
-app.on('ready', createWindow)
-
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on('ready', () =>
+{
+  createWindow();
+  if(isWindows)
+  {
+    console.log("isWindows");
   }
-})
+});
+
+app.on('window-all-closed', () =>
+{
+  if (!isMac)
+  {
+    app.quit();
+  }
+});
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
+  if (win === null)
+  {
+    createWindow();
   }
-})
+});
